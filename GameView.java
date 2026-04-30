01547f1aafe04f02c69518ddebc457393b5436e1
so-atJapan <uchidasou1206@gmail.com>
2026-04-30 20:48:10 +0900

    Add files via upload

---
+++ GameView.java
@@ -0,0 +1,34 @@
+package View;
+
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class GameView extends JFrame {
+    JFrame frame;
+    GridPanel gridPanel;
+    HintPanel hintPanel;
+    JButton[][] buttons;
+
+    public void render(){
+        setTitle("MVC Counter");
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+
+        buttons
+
+        add(label);
+        add(button);
+    }
+
+    public void updateCell(int x, int y){
+
+    }
+
+    public void showResult(boolean result){
+
+    }
+}
