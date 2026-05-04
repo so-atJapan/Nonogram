fdc34d3ea189de08852fa509cbe7f9a94df6d868
so-atJapan <uchidasou1206@gmail.com>
2026-05-04 21:53:40 +0900

    Create Grid.java

---
+++ model/Grid.java
@@ -0,0 +1,49 @@
+package Nonogram.model;
+
+public class Grid {
+    private int sizeX;
+    private int sizeY;
+    private Cell[][] cells;
+
+    public Grid(int sizeX, int sizeY){
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+    }
+    public Grid(int sizeX, int sizeY, String[][] cells){
+        this(sizeX, sizeY);
+
+        this.cells = new Cell[sizeX][sizeY];
+        for (int x = 0; x < sizeX; x++) {
+            for (int y = 0; y < sizeY; y++) {
+                switch (cells[x][y]) {
+                    case "0":
+                        this.cells[x][y] = new Cell(CellState.EMPTY);
+                        break;
+                    case "1":
+                        this.cells[x][y] = new Cell(CellState.FILLED);
+                        break;
+                    case "2":
+                        this.cells[x][y] = new Cell(CellState.MARKED);
+                        break;
+                }
+            }
+        }
+    }
+
+    //ゲッター
+    public int getSizeX() {return sizeX;}
+    public int getSizeY() {return sizeY;}
+
+
+    public Cell getCellAt(int x, int y){
+        return cells[x][y];
+    }
+    public Cell[][] getCellAll(){
+        return cells;
+    }
+
+    public void setCellAt(int x, int y, CellState cellState){
+        this.cells[x][y] = new Cell(cellState);
+    }
+
+}
