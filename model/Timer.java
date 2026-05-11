5a91d70b1fc0d0432a944cea565f5d63c3a64775
Itsuki-O <itsuitsublue623@gmail.com>
2026-05-11 16:54:21 +0900

    Add files via upload

---
+++ model/Timer.java
@@ -0,0 +1,24 @@
+package Nonogram.model;
+
+public class Timer {
+
+
+    private int timeCount ;
+
+    /**
+     * コンストラクタ
+     */
+    public Timer() {
+        this.timeCount = 0;
+    }
+
+    /**
+     * 一秒ごとに呼ばれる処理
+     * 
+     * @return 経過時間
+     */
+    public int tick() {
+        timeCount++;
+        return timeCount;
+    }
+}
