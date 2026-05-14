ed8cd7e859fa6114d121f52f90040f163b817d62
so-atJapan <uchidasou1206@gmail.com>
2026-05-14 17:28:28 +0900

    Update GameController.java

--- controller/GameController.java
+++ controller/GameController.java
@@ -1,110 +1,87 @@
 package Nonogram.controller;
 
+
 import Nonogram.model.CellState;
+import Nonogram.model.GameModel;
 import Nonogram.model.Puzzle;
-import Nonogram.model.PuzzleEditorModel;
-import Nonogram.view.PuzzleEditorView;
+import Nonogram.model.Timer;
+import Nonogram.view.GameView;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.util.Duration;
 
-public class PuzzleEditorController {
+public class GameController {
 
-    private PuzzleEditorModel model;
-    private PuzzleEditorView view;
+    private GameModel model;
+    private GameView view;
+    Timer timer = new Timer();
+    private Timeline timeline;
 
-    /**
-     * コンストラクタ
-     *
-     * @param model 
-     * @param view  
-     */
-    public PuzzleEditorController(PuzzleEditorModel model, PuzzleEditorView view) {
+    // ドラッグ中に使用するマウスボタンを記憶
+    private MouseButton dragButton = null;
+
+    public GameController(GameModel model, GameView view) {
         this.model = model;
         this.view  = view;
-
     }
 
-    /**
-     * ゲームを起動
-     * ボタン描画呼び出し、初期化
-     */
     public void initialize() {
-        // PuzzleのデータをViewに渡す
         view.initialize(model.getPuzzle());
-        
-        // 左クリック
-        // 画面ボタン
-        // ボタンを押したら受け取る
+        view.render();
+
         Puzzle puzzle = model.getPuzzle();
         for (int x = 0; x < puzzle.getGridSizeX(); x++) {
             for (int y = 0; y < puzzle.getGridSizeY(); y++) {
                 int finalX = x;
                 int finalY = y;
-                view.getButtons()[finalX][finalY].setOnAction(e -> onCellClicked(finalX, finalY));
+
+                // クリック
+                view.getButtons()[finalX][finalY].setOnMouseClicked((e) -> {
+                    if (e.getButton() == MouseButton.PRIMARY) {
+                        onCellLeftClicked(finalX, finalY);
+                    } else if (e.getButton() == MouseButton.SECONDARY) {
+                        onCellRightClicked(finalX, finalY);
+                    }
+                });
+
+                // ドラッグ開始：押したボタンを記憶してフルドラッグ開始
+                view.getButtons()[finalX][finalY].setOnDragDetected((e) -> {
+                    dragButton = e.getButton();
+                    view.getButtons()[finalX][finalY].startFullDrag();
+                });
+
+                // ドラッグ中になぞったセルに対して処理
+                view.getButtons()[finalX][finalY].setOnMouseDragEntered((e) -> {
+                    if (dragButton == MouseButton.PRIMARY) {
+                        onCellLeftClicked(finalX, finalY);
+                    } else if (dragButton == MouseButton.SECONDARY) {
+                        onCellRightClicked(finalX, finalY);
+                    }
+                });
             }
         }
-        
-        // リセットボタン
-        // view.getResetButton().setOnAction(e -> onReset());
-        
-        // 設定ボタン
-        view.getSettingButton().setOnAction(e -> view.semiModalRender(model.getPuzzle()));
-        
-        //　OKボタン
-        view.getOkButton().setOnAction(e -> onSettingConfirm());
-        
-        // チェックボタン
-        view.getCheckButton().setOnAction(e -> onCheck());
-        
-        // 描画
-        view.render();
-        view.semiModalRender(model.getPuzzle());
+
+        view.getCheckButton().setOnAction(e -> onJudge());
+
+        timeline = new Timeline(new KeyFrame(Duration.seconds(1), e -> view.updateTimer(timer.tick())));
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
     }
 
-    /**
-     * セルが左クリックされたときの処理。
-     *
-     * @param x クリックされたセルのX座標
-     * @param y クリックされたセルのY座標
-     */
-    public void onCellClicked(int x, int y) {
+    public void onCellLeftClicked(int x, int y) {
         model.toggle(x, y, CellState.FILLED);
         view.updateCell(x, y, model.getGrid());
     }
 
-    /**
-     * 設定の決定ボタンが押されたときの処理。
-     */
-    public void onSettingConfirm() {
-        model.updatePuzzleTitle(view.getTitleTextField());
-        model.updatePuzzleGridSizeX(view.getGridSizeX());
-        model.updatePuzzleGridSizeY(view.getGridSizeY());
-        model.gridReSize();
-        view.gridReSize(model.getGrid());
-        view.settingConfirm();
-
-        Puzzle puzzle = model.getPuzzle();
-        for (int x = 0; x < puzzle.getGridSizeX(); x++) {
-            for (int y = 0; y < puzzle.getGridSizeY(); y++) {
-                int finalX = x;
-                int finalY = y;
-                view.getButtons()[finalX][finalY].setOnAction(e -> onCellClicked(finalX, finalY));
-            }
-        }
-
-        view.render();
+    public void onCellRightClicked(int x, int y) {
+        model.toggle(x, y, CellState.MARKED);
+        view.updateCell(x, y, model.getGrid());
     }
 
-    /**
-     * リセットボタンが押されたときの処理。
-     */
-    public void onReset() {
-        model.reset();
-        view.updateCell(0, 0, model.getGrid());
-    }
-
-    /**
-     * チェックボタンが押されたときの処理。
-     */
-    public void onCheck() {
-        model.updateDB();
+    public void onJudge() {
+        boolean result = model.check();
+        view.showResult(result);
     }
 }
