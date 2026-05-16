7e1322edfc2073b89d36a76edd65a7c61615145f
so-atJapan <uchidasou1206@gmail.com>
2026-05-16 13:34:41 +0900

    Add files via upload

--- controller/ResultController.java
+++ controller/ResultController.java
@@ -3,6 +3,10 @@ package Nonogram.controller;
 import Nonogram.model.ResultModel;
 import Nonogram.view.ResultView;
 
+/**
+ * リザルト画面の処理を管理するコントローラクラス
+ * 太田
+ */
 public class ResultController {
 
     private ResultModel model;
@@ -10,12 +14,22 @@ public class ResultController {
     private AppController appController;
     // private PlayRecord record; // TODO: PlayRecordクラス実装後にResultModelの代わりに利用予定
 
+    /**
+     * コンストラクタ
+     * 
+     * @param model リザルト画面で使用するデータ
+     * @param view リザルト画面のView
+     * @param appController 画面遷移を管理するコントローラ
+     */
     public ResultController(ResultModel model, ResultView view, AppController appController) {
         this.model = model;
         this.view = view;
         this.appController = appController;
     }
 
+    /**
+     * リザルト画面を初期化し、ボタンイベントを登録する
+     */
     public void initialize() {
         view.initialize(model);
         view.render();
@@ -24,10 +38,16 @@ public class ResultController {
         // view.showRetryButton().setOnAction(e -> onRetry()); // TODO: リトライ機能追加時に有効化
     }
 
+    /**
+     * リザルト内容をViewに表示する
+     */
     public void showResult() {
         view.displayResult(model);
     }
 
+    /**
+     * 問題リスト画面へ戻る
+     */
     public void onPuzzleList() {
         appController.navigateTo("list");
     }
--- model/Timer.java
+++ model/Timer.java
@@ -1,5 +1,9 @@
 package Nonogram.model;
 
+/**
+ * ゲーム中の経過時間を管理するクラス
+ * 太田
+ */
 public class Timer {
 
 
@@ -22,7 +26,12 @@ public class Timer {
         return timeCount;
     }
 
-    public int getElapsedSeconds() {
+    /**
+     * 現在の経過秒数を取得する
+     * 
+     * @return 経過秒数
+     */
+    public int getTickSeconds() {
         return timeCount;
     }
 }
